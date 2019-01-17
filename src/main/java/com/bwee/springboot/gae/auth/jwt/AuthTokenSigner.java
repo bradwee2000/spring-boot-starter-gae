d2e6d9a3bf1908c9cee4e0057960f1ea61900766
Bradford Wee <bradwee2000@gmail.com>
2019-01-17 10:25:58 +0800

    accept generic AuthUser

--- src/main/java/com/bwee/springboot/gae/auth/jwt/AuthTokenSigner.java
+++ src/main/java/com/bwee/springboot/gae/auth/jwt/AuthTokenSigner.java
@@ -2,23 +2,23 @@ package com.bwee.springboot.gae.auth.jwt;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
-import com.bwee.springboot.gae.auth.user.SimpleAuthUser;
+import com.bwee.springboot.gae.auth.user.AuthUser;
 
 /**
  * @author bradwee2000@gmail.com
  */
-public class AuthTokenSigner {
+public class AuthTokenSigner<T extends AuthUser> {
 
   private final Algorithm signingAlgorithm;
-  private final TokenTranslator tokenTranslator;
+  private final TokenTranslator<T> tokenTranslator;
 
   public AuthTokenSigner(final Algorithm signingAlgorithm,
-                         final TokenTranslator tokenTranslator) {
+                         final TokenTranslator<T> tokenTranslator) {
     this.signingAlgorithm = signingAlgorithm;
     this.tokenTranslator = tokenTranslator;
   }
 
-  public String signToken(final SimpleAuthUser user) {
+  public String signToken(final T user) {
     return tokenTranslator.toJwt(user, JWT.create()).sign(signingAlgorithm);
   }
 }

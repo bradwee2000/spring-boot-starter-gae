95762c0d15b543dd29eeb65aa84615e7d186df63
Bradford Wee <bradwee2000@gmail.com>
2019-02-21 13:06:23 +0800

    add interface on AbstractService

--- src/main/java/com/bwee/springboot/gae/model/service/AbstractService.java
+++ src/main/java/com/bwee/springboot/gae/model/service/AbstractService.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 /**
  * @author bradwee2000@gmail.com
  */
-public class AbstractService<K, T> {
+public class AbstractService<K, T> implements ModelService<K, T>{
 
   private final AbstractDao<K, T, ?> dao;
 
@@ -18,54 +18,67 @@ public class AbstractService<K, T> {
     this.dao = dao;
   }
 
+  @Override
   public Optional<T> findById(final K id) {
     return dao.findOne(id);
   }
 
+  @Override
   public List<T> findByIds(final Collection<K> ids) {
     return dao.findByIds(ids);
   }
 
+  @Override
   public List<T> findByIds(final K id, final K ... more) {
     return findByIds(Lists.asList(id, more));
   }
 
+  @Override
   public boolean isExists(final K id) {
     return dao.isExists(id);
   }
 
+  @Override
   public List<T> findAll() {
     return dao.findAll();
   }
 
+  @Override
   public List<T> findAllByPage(final int offset, final int limit) {
     return dao.findAllByPage(offset, limit);
   }
 
+  @Override
   public T save(final T value) {
     return saveAll(value).stream().findFirst().get();
   }
 
+  @Override
   public List<T> saveAll(final Collection<T> values) {
     return  dao.saveAll(values);
   }
 
+  @Override
   public List<T> saveAll(final T value, final T ... more) {
     return saveAll(Lists.asList(value, more));
   }
 
+  @Override
   public List<K> deleteAll(final Collection<K> ids) {
     return dao.deleteAll(ids);
   }
 
+  @Override
   public List<K> deleteAll(final K id, final K ... more) {
     return deleteAll(Lists.asList(id, more));
   }
 
+  @Override
   public List<K> deleteAll() {
     return dao.deleteAll();
   }
 
+  @Override
   public K delete(final K id) {
     return deleteAll(id).stream().findFirst().get();
   }
---
+++ src/main/java/com/bwee/springboot/gae/model/service/ModelService.java
@@ -0,0 +1,37 @@
+package com.bwee.springboot.gae.model.service;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * @author bradwee2000@gmail.com
+ */
+interface ModelService<K, T> {
+
+  Optional<T> findById(final K id);
+
+  List<T> findByIds(final Collection<K> ids);
+
+  List<T> findByIds(final K id, final K ... more);
+
+  boolean isExists(final K id);
+
+  List<T> findAll();
+
+  List<T> findAllByPage(final int offset, final int limit);
+
+  T save(final T value);
+
+  List<T> saveAll(final Collection<T> values);
+
+  List<T> saveAll(final T value, final T ... more);
+
+  List<K> deleteAll(final Collection<K> ids);
+
+  List<K> deleteAll(final K id, final K ... more);
+
+  List<K> deleteAll();
+
+  K delete(final K id);
+}
